d23d4b93fdeb0f9ea5f5d54faf1b40547a42fe4f
Richard Thomas <richardhthomas@yahoo.com>
2013-09-27 12:17:12 +0100

    27/09/13 html mail - work in progress

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -9,14 +9,14 @@ class Notifier < ActionMailer::Base
   def sign_up(user, ampm)
     @greeting = "Hi " + user.email
     if ampm.nil?
-      @next_reading_1 = "Please go to <%= link_to 'HomeBloodPressure.co.uk', new_user_session_path %> to take a pair of blood pressure readings."
+      @next_reading_1 = "Please go to" + link_to 'HomeBloodPressure.co.uk', new_user_session_path + "to take a pair of blood pressure readings."
       @next_reading_2 = "So, please go to <%= link_to 'HomeBloodPressure.co.uk', new_user_session_path %> to take a pair of blood pressure readings."
     elsif ampm == 'am'
       @next_reading_1 = "Please go to <%= link_to 'HomeBloodPressure.co.uk', new_user_session_path %> to take another pair of blood pressure readings this evening."
       @next_reading_2 = "So, this evening, please go to <%= link_to 'HomeBloodPressure.co.uk', new_user_session_path %> to take another pair of blood pressure readings."
     else
-      @next_reading_1 = "Please go to <%= link_to 'HomeBloodPressure.co.uk', new_user_session_path %> to take another pair of blood pressure readings tomorrow morning."
-      @next_reading_2 = "So, tomorrow morning, please go to <%= link_to 'HomeBloodPressure.co.uk', new_user_session_path %> to take another pair of blood pressure readings."
+      @next_reading_1 = "Please go to" + link_to 'HomeBloodPressure.co.uk', new_user_session_path + "to take another pair of blood pressure readings tomorrow morning."
+      @next_reading_2 = "So, tomorrow morning, please go to" + link_to 'HomeBloodPressure.co.uk', new_user_session_path + "to take another pair of blood pressure readings."
     end
 
     mail to: user.email, subject: "Welcome to HomeBloodPressure.co.uk"
