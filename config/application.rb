7b05408ccd18787d890b21d75f286d6585604fe9
Richard Thomas <richardhthomas@yahoo.com>
2013-09-27 17:30:42 +0100

    27/09/13 html mail 7

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module Homebp
     # postmark settings
     config.action_mailer.delivery_method   = :postmark
     config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
-    config.action_mailer.default_url_options = { host: "http://guarded-thicket-5724.herokuapp.com/" }
+    config.action_mailer.default_url_options = { host: "http://guarded-thicket-5724.herokuapp.com" }
   end
 end
