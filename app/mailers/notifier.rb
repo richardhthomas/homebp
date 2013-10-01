a057d5e7e7d225aafcd808df00d4348237418fe4
Richard Thomas <richardhthomas@yahoo.com>
2013-10-01 12:13:18 +0100

    devise to use postmark

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,4 +1,6 @@
 class Notifier < ActionMailer::Base
+  include Devise::Mailers::Helpers
+  
   default from: "richard.thomas@itamus.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -6,6 +8,19 @@ class Notifier < ActionMailer::Base
   #
   #   en.notifier.sign_up.subject
   #
+  
+  def confirmation_instructions(record)
+    devise_mail(record, :confirmation_instructions)
+  end
+
+  def reset_password_instructions(record)
+    devise_mail(record, :reset_password_instructions)
+  end
+
+  def unlock_instructions(record)
+    devise_mail(record, :unlock_instructions)
+  end
+  
   def sign_up(user, ampm)
     @greeting = "Hi " + user.email
     if ampm.nil?
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -243,4 +243,7 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+  
+  # Use my Notifier mailer class for sending mail - so uses postmark.
+  config.mailer = "Notifier"
 end
