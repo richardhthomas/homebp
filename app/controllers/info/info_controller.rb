dbacb0b1a55243389f2339801be0e13080548b4e
Richard Thomas <richardhthomas@yahoo.com>
2013-12-28 18:20:59 +0000

    routes defined but all files not yet created

--- app/controllers/info/info_controller.rb
+++ app/controllers/info/info_controller.rb
@@ -1,19 +1,96 @@
 class Info::InfoController < ApplicationController
   before_action :set_cache_buster
   before_action :set_date_ampm
-  before_action :collect_bp_entry_details, only: [:landing_page, :measuring_blood_pressure, :treating_blood_pressure, :what_is_blood_pressure]
-  before_action :collect_bp, only: [:landing_page, :measuring_blood_pressure, :treating_blood_pressure, :what_is_blood_pressure]
+  before_action :collect_bp_entry_details
+  before_action :collect_bp
   
   def landing_page
   end
   
+  
   def measuring_blood_pressure
   end
   
-  def treating_blood_pressure
-  end
+    def how_do_i_measure_my_blood_pressure
+    end
+  
+    def when_should_i_measure_my_blood_pressure
+    end
+  
+    def how_do_i_choose_a_blood_pressure_machine
+    end
+  
   
   def what_is_blood_pressure
   end
+  
+    def what_am_i_measuring
+    end
+    
+    def what_do_the_numbers_mean
+    end
+    
+    def what_is_the_normal_range_for_me
+    end
+  
+    
+  def treating_blood_pressure
+  end
+  
+    def lifestyle_options
+    end
+    
+      def diet
+      end
+      
+      def exercise
+      end
+      
+      def smoking
+      end
+      
+    
+    def complementary_therapy
+    end
+    
+      def supplements
+      end
+      
+      def acupuncture
+      end
+      
+      def herbal
+      end
+      
+    
+    def medication
+    end
+
+      def when_is_medication_recommended
+      end
+      
+      def why_do_i_need_to_take_more_than_one_medication
+      end
+      
+      def how_can_i_reduce_the_risk_of_side_effects
+      end
+      
+      def what_are_the_different_medication_options_for_me
+      end
+      
+        def calcium_channel_blockers
+        end
+        
+        def ace_inhibitors
+        end
+        
+        def diuretics
+        end
+        
+        def a2rbs
+        end
+        
+        def beta_blockers
+        end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,49 @@ Homebp::Application.routes.draw do
     get 'measuring_blood_pressure'
     get 'treating_blood_pressure'
     
-    scope '/measuring', :controller => "measuring" do
+    scope '/measuring', :controller => "info" do
       get 'how_do_i_measure_my_blood_pressure'
       get 'when_should_i_measure_my_blood_pressure'
       get 'how_do_i_choose_a_blood_pressure_machine'
+    end
+    
+    scope '/what', :controller => "info" do
+      get 'what_am_i_measuring'
+      get 'what_do_the_numbers_mean'
+      get 'what_is_the_normal_range_for_me'
+    end
+    
+    scope '/treatment', :controller => "info" do
+      get 'lifestyle_options'
+      get 'complementary_therapy'
+      get 'medication'
+      
+      scope '/lifestyle', :controller => "info" do
+        get 'diet'
+        get 'exercise'
+        get 'smoking'
+      end
+      
+      scope '/complementary', :controller => "info" do
+        get 'supplements'
+        get 'acupuncture'
+        get 'herbal'
+      end
+      
+      scope '/medication', :controller => "info" do
+        get 'when_is_medication_recommended'
+        get 'why_do_i_need_to_take_more_than_one_medication'
+        get 'how_can_i_reduce_the_risk_of_side_effects'
+        get 'what_are_the_different_medication_options_for_me'
+        
+        scope '/options', :controller => "info" do
+          get 'calcium_channel_blockers'
+          get 'ace_inhibitors'
+          get 'diuretics'
+          get 'a2rbs'
+          get 'beta_blockers'
+        end
+      end
     end
   end
     
